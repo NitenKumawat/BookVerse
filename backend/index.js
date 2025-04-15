dc3d525ffb9c6891275a2fb106f303b9e4c3ac62
Niten Kumawat <nitenkumawat@gmail.com>
2025-04-15 22:27:28 +0530

    deployment  on render and vercel

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "book-verse-flax.vercel.app", credentials: true }));
 
 // API Routes
 app.use("/api/auth", authRoutes);
